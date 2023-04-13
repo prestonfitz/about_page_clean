3e9f107c721e2c59d29bbecadce4fe46ed3e082e
Preston Fitzgerald <prestonfitz@gmail.com>
2023-04-13 12:17:49 -0600

    Again with the hosts

--- about_page/about_page/settings.py
+++ about_page/about_page/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://aboutpageclean-production.up.railway.app',
-                 '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['https://aboutpageclean-production.up.railway.app']
 
 
 # Application definition
